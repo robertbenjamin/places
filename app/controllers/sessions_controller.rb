adc1e22039f5d6fcfd905adceac70b7104f1427f
Robert <i.am.robert.benjamin@gmail.com>
2015-09-26 23:39:15 -0400

    Clean up files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,19 @@
 class SessionsController < ApplicationController
-  # def new
-  # end
 
   def create
   user = User.find_by_username(params[:username])
-	  if user && user.authenticate(params[:password])
-	    session[:user_id] = user.id
-	    redirect_to user_places_path(current_user)
-	  else
-	    redirect_to login_path, notice: "Username or password is invalid."
-	  end
-	end
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_places_path(current_user)
+    else
+      redirect_to login_path, notice: "Username or password is invalid."
+    end
+  end
 
-	def destroy
-  	session[:user_id] = nil
-  	redirect_to root_url
-  	# attach to above line if you want a notice => , notice: "Logged out!"
-	end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+    # attach to above line if you want a notice => , notice: "Logged out!"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,57 +12,10 @@ class UsersController < ApplicationController
     @user = User.new(params.require(:user).permit(:username, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_places_path(current_user), notice: "Thank you for signing up!"
+      redirect_to user_places_path(current_user), notice: "Thank you for signing up — try adding a new place above!"
     else
       render "new"
     end
-end
-
-
-  # def sign_up
-  # end
-
-  # def sign_up!
-  #   user = User.new(
-  #     username: params[:username],
-  #     password_digest: BCrypt::Password.create(params[:password])
-  #   )
-  #   if params[:password_confirmation] != params[:password]
-  #     message = "Your passwords don't match!"
-  #   elsif user.save
-  #     message = "Your account has been created!"
-  #   else
-  #     message = "Your account couldn't be created. Did you enter a unique username and password?"
-  #   end
-  #   flash[:notice] = message
-  #   redirect_to action: :sign_up
-  # end
-
-  # def sign_in
-  # end
-
-  # def sign_in!
-  #   @user = User.find_by(username: params[:username])
-  #   if !@user
-  #     message = "This user doesn't exist!"
-  #   elsif !BCrypt::Password.new(@user.password_digest).is_password?(params[:password])
-  #     message = "Your password's wrong!"
-  #   else
-  #     message = "Welcome, #{@user.username}! "
-  #       cookies[:username] = {
-  #         value: @user.username
-  #         # expires: 100.years.from_now
-  #       }
-  #       session[:user] = @user
-  #   end
-  #   flash[:notice] = message
-  #   redirect_to root_url
-  # end
-
-  # def sign_out
-  #   flash[:notice] = "You're signed out!"
-  #   reset_session
-  #   redirect_to root_url
-  # end
+  end
 
 end
